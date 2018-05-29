61990f12320107440b55bad1eb0a7d4f2ebf31ae
develpet <mrlouboltz@gmail.com>
2018-05-29 09:06:29 +0200

    Controllo password con hash

--- seavision/js/index.js
+++ seavision/js/index.js
@@ -1,6 +1,28 @@
  $("#login-button").click(function(event){
 		 event.preventDefault();
 	 
-	 $('form').fadeOut(500);
-	 $('.wrapper').addClass('form-success');
+	 var pwd = document.getElementById('inpwd').value;
+	 var hash = pwd.hashCode();
+	 
+	 if (hash == '1216985755')
+	 {
+		 $("#welcome-form").fadeOut(500);
+		 //$('.wrapper').addClass('form-success');
+		 
+		 $("#download-form").css('opacity', 1).fadeIn();
+	 }
+
 });
+
+String.prototype.hashCode = function() {
+    var hash = 0;
+    if (this.length == 0) {
+        return hash;
+    }
+    for (var i = 0; i < this.length; i++) {
+        var char = this.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
